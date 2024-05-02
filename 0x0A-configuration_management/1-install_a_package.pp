1f167c89b3a53d014be12bad87bc71cb0825a803
eyob721 <eyobmelkamu721@gmail.com>
2024-05-02 23:27:16 +0300

    fix: install werkzeug too

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # Install flask from pip3
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    name     => 'werkzeug',
+    provider => 'pip3',
+}
+
 package { 'flask':
-    ensure   => 'installed',
-    name     => 'flask==2.1.0',
+    ensure   => '2.1.0',
+    name     => 'flask',
     provider => 'pip3',
 }
