11fa26019683f165d9b4361472f786a644116ee4
eyob721 <eyobmelkamu721@gmail.com>
2024-05-08 01:39:52 +0300

    fix: remove ensure attribute

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 $ssh_path='/etc/ssh/ssh_config'
 
 file_line { 'Turn off passwd auth':
-    ensure => present,
-    path   => $ssh_path,
-    line   => 'PasswordAuthentication no',
+    path => $ssh_path,
+    line => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-    ensure => present,
-    path   => $ssh_path,
-    line   => 'IdentityFile ~/.ssh/school',
+    path => $ssh_path,
+    line => 'IdentityFile ~/.ssh/school',
 }
